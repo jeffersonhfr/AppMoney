0fce11e1f452d844881d86d9f2d37a4817d9ac1d
Jefferson Rodrigues <jeffersonhfr@gmail.com>
2022-03-09 14:51:38 -0300

    feature: installing react-modal and setting modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import Modal from 'react-modal';
 import { GlobalStyle } from './styles/global';
+import { useState } from 'react';
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <Header onOpenNewTransactionsModal={handleOpenNewTransactionModal} />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>MODAL</h2>
+      </Modal>
+
       <Dashboard />
     </>
   );
